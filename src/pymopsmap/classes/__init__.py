a9e0be86e36715d890a9ce4dc6ac8f77e6686d7f
walcark <kevin.walcarius@cnes.fr>
2025-11-19 17:53:12 +0100

    feat: write the __init__.py file for the classes submodules.

---
+++ src/pymopsmap/classes/__init__.py
@@ -0,0 +1,5 @@
+from .psd import FileDefinedPSD, FixedPSD, LognormalPSD, ModifiedGammaPSD
+from .shape import Irregular, Sphere, Spheroid, SpheroidLognormal
+from .wavelength import Wavelength, wavelength
+from .refractive_index import RefractiveIndex
+from .mode import Mode
